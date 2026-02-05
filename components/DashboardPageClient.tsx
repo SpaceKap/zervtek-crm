807406ed245f946fa2e2f4e88d3bd45759306e2a
SpaceKap <spacekapsule@gmail.com>
2026-02-05 12:40:27 +0900

    Fix DashboardPageClient: add missing currentUserEmail prop

--- components/DashboardPageClient.tsx
+++ components/DashboardPageClient.tsx
@@ -24,6 +24,7 @@ export function DashboardPageClient({
   isAdmin,
   users,
   currentUserId,
+  currentUserEmail,
   showUnassignedOnly,
 }: DashboardPageClientProps) {
   const [filterUserId, setFilterUserId] = useState<string>("all");
